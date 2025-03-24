3cf0b5027ba133f2caf162e5c69c75461fa41624
Bomba004 <alipomp004@gmail.com>
2025-03-24 20:48:21 +0200

    commit-001

--- todo.ts
+++ todo.ts
@@ -3,18 +3,18 @@
 
 /* GitHub:-
 
-// Initialize Git | تهيئة Git
-  git init
-// Add all files | إضافة جميع الملفات
-  git add .
-// Commit | تسجيل
-  git commit -m "Initial commit"
-// Check remote repositories | عرض المواقع البعيدة
-  git remote -v
+  // Initialize Git | تهيئة Git
+git init
+  // Add all files | إضافة جميع الملفات
+git add .
+  // Commit | تسجيل
+git commit -m "commit-001"
 // Add remote repository | لربط المشروع بـ GitHub
-  git remote add origin git@github.com:Bomba004/react-auth.git
-// Push to remote repository | إرسال إلى المواقع البعيدة
-  git push -u origin main
+git remote add origin git@github.com:Bomba004/react-auth.git
+// Check remote repositories | عرض المواقع البعيدة
+git remote -v
+  // Push to remote repository | إرسال إلى المواقع البعيدة
+git push -u origin main
 
 
 
