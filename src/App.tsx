7adb965c0915264cfc11da56d0f3dcf19d6c1706
Bomba004 <alipomp004@gmail.com>
2025-03-25 02:18:39 +0200

    003

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import { toast } from "react-toastify";
 import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
 
-const routerApi = '/react-auth/';
+const routerApi = '/react-auth';
 
 const App: React.FC = () => {
 
@@ -43,19 +43,19 @@ const App: React.FC = () => {
         {user && <Navbar />}
         <main className={user ? 'container mx-auto px-4 py-8 mt-6' : ''}>
           <Routes>
-            <Route path={routerApi + "login"} element={
-              user ? <Navigate to={routerApi + "dashboard"} /> : <Login />
+            <Route path={routerApi + "/login"} element={
+              user ? <Navigate to={routerApi + "/dashboard"} /> : <Login />
             } />
             <Route
-              path={routerApi + "dashboard"}
+              path={routerApi + "/dashboard"}
               element={
                 <PrivateRoute>
                   <Dashboard />
                 </PrivateRoute>
               }
             />
-            <Route path={routerApi + "error"} element={<ErrorPage />} />
-            <Route path={routerApi + ""} element={<Navigate to={routerApi + "dashboard"} />} />
+            <Route path={routerApi + "/error"} element={<ErrorPage />} />
+            <Route path={routerApi + ""} element={<Navigate to={routerApi + "/dashboard"} />} />
             <Route path="*" element={<ErrorPage title="Error 404" message="Page not found" />} />
           </Routes>
         </main>
