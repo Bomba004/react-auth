396884c24ce2de49b739c00aabcd8c0b996c6e1c
Bomba004 <alipomp004@gmail.com>
2025-04-28 09:42:31 +0300

    Updates

---
+++ assets/Home-CvmG3pJO.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CJVl8CjU.js";const o=()=>e.jsxs(e.Fragment,{children:[e.jsx("h1",{children:"Home"}),e.jsx("p",{children:"welcome BomBa"})]});export{o as Home,o as default};
