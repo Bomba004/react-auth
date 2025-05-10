f5f2b59f1131689a2cf7e38e4db6c8eb2a9b51da
Bomba004 <alipomp004@gmail.com>
2025-05-10 11:42:02 +0300

    Updates

--- assets/Home-DqoGRHck.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-B8bfNg_-.js";const o=()=>e.jsxs(e.Fragment,{children:[e.jsx("h1",{children:"Home"}),e.jsx("p",{children:"welcome BomBa"})]});export{o as Home,o as default};
